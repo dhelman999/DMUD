fdb2a3c8947c08a4c82491bb032fe74dc2b2015f
MrGrimm999 <MrGrimm999@8e71f4b0-c50f-4f4d-bad6-908c98c512d7>
2013-11-15 04:12:14 +0000

    git-svn-id: https://subversion.assembla.com/svn/dmud.8th_Circle/trunk@3 8e71f4b0-c50f-4f4d-bad6-908c98c512d7

---
+++ 8th_Circle_Server/8th_Circle_Server/CommandHandler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace _8th_Circle_Server
+{
+    enum commandType
+    {
+        VERB=1,
+        NOUN,
+        PREP
+    };
+
+    struct Command
+    {
+        public string command;
+        public int matchNumber;
+        public commandType type;
+
+        public Command(string command, int matchNumber, commandType type)
+        {
+            this.command = command;
+            this.matchNumber = matchNumber;
+            this.type = type;
+        }
+    }
+
+    class CommandHandler
+    {
+        ArrayList mCommandList;
+        ArrayList mNounList;
+        ArrayList mVerbList;
+        ArrayList mPrepList;
+
+        public CommandHandler()
+        {
+            mCommandList = new ArrayList();
+            mVerbList = new ArrayList();
+            mNounList = new ArrayList();
+            mPrepList = new ArrayList();
+
+            Command pt;
+
+            pt = new Command("move", 1, commandType.VERB);
+            mCommandList.Add(pt);
+            mVerbList.Add(pt);
+
+            pt = new Command("look", 1, commandType.VERB);
+            mCommandList.Add(pt);
+            mVerbList.Add(pt);
+
+            pt = new Command("exit", 1, commandType.VERB);
+            mCommandList.Add(pt);
+            mVerbList.Add(pt);
+        }
+    }
+}
