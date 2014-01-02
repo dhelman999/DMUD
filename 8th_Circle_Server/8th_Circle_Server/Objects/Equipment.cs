deecabd27368fd0f22d4927cff2f0090954da8c7
MrGrimm999 <MrGrimm999@8e71f4b0-c50f-4f4d-bad6-908c98c512d7>
2014-01-02 16:41:45 +0000

    just moving around files/folders
    git-svn-id: https://subversion.assembla.com/svn/dmud.8th_Circle/trunk@100 8e71f4b0-c50f-4f4d-bad6-908c98c512d7

---
+++ 8th_Circle_Server/8th_Circle_Server/Objects/Equipment.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace _8th_Circle_Server
+{
+    public class Equipment : Mob
+    {
+        public int mLevel;
+        public EQType mType;
+        public EQSlot mSlot;
+        public Useable mUsedby;
+        public int mHitMod;
+        public int mMinDam;
+        public int mMaxDam;
+        public int mArmor;
+        public int mPhysRes;
+        public int mMaxPhysResBoost;
+        public int mFireRes;
+        public int mMaxFireResBoost;
+        public int mColdRes;
+        public int mMaxColdResBoost;
+        public int mLightningRes;
+        public int mMaxLightningResBoost;
+        public int mAcidRes;
+        public int mMaxAcidResBoost;
+        public int mForceRes;
+        public int mMaxForceResBoost;
+        public ArrayList mEffects;
+
+        public Equipment() : base()
+        {
+            mLevel = 1;
+            mEffects = new ArrayList();
+        }// constructor
+
+        public Equipment(Equipment eq) : base(eq)
+        {
+            mLevel = eq.mLevel;
+            mType = eq.mType;
+            mSlot = eq.mSlot;
+            mUsedby = eq.mUsedby;
+            mHitMod = eq.mHitMod;
+            mMinDam = eq.mMinDam;
+            mMaxDam = eq.mMaxDam;
+            mArmor = eq.mArmor;
+            mPhysRes = eq.mPhysRes;
+            mMaxPhysResBoost = eq.mMaxPhysResBoost;
+            mFireRes = eq.mFireRes;
+            mMaxFireResBoost = eq.mMaxFireResBoost;
+            mColdRes = eq.mColdRes;
+            mMaxColdResBoost = eq.mMaxColdResBoost;
+            mLightningRes = eq.mLightningRes;
+            mMaxLightningResBoost = eq.mMaxLightningResBoost;
+            mAcidRes = eq.mAcidRes;
+            mMaxAcidResBoost = eq.mMaxAcidResBoost;
+            mForceRes = eq.mForceRes;
+            mMaxForceResBoost = eq.mMaxForceResBoost;
+            mEffects = (ArrayList)eq.mEffects.Clone();
+        }// copy constructor
+
+        public override string wear(Player pl)
+        {
+            if (pl.mEQList[(int)mSlot] == null)
+            {
+                pl.mEQList[(int)mSlot] = this;
+                pl.mInventory.Remove(this);
+                return "you equip the " + mName;
+            }// if
+            else
+                return "you are already wearing the " + ((Mob)pl.mEQList[(int)mSlot]).mName;
+        }// wear
+
+        public override string remove(Player pl)
+        {
+            if (pl.mEQList[(int)mSlot] == null)
+                return "";
+            else
+            {
+                pl.mEQList[(int)mSlot] = null;
+                pl.mInventory.Add(this);
+                return "you remove the " + mName;
+            }// else
+        }// wear
+
+    }// class Equipment
+
+}// namespace _8th_Circle_Server
